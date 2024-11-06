0b9fb1c99baa1aa68ce6cd6a4f0730dc40de55a3
zoritto <zoritto@zoritto.zoritto>
2024-11-06 07:46:53 -0300

    criando novo arquivo index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require_once('config.php');
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prontuário Veterinario</title>
+</head>
+<body>
+    
+</body>
+</html>
